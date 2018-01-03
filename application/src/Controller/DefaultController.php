17762eb647f21998793a713e951ff37fedff515c
Gerard Costa <viscat88@gmail.com>
2018-01-03 13:57:02 +0100

    Add Symfony4 demo application

---
+++ application/src/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function index()
+    {
+        return new Response('Hi!');
+    }
+}
